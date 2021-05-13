1989db5206346e74a46547576049830f34d8677c
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-12 23:16:49 -0300

    test: add test to findByName function in product repository

--- src/modules/products/typeorm/repositories/products.repository.spec.ts
+++ src/modules/products/typeorm/repositories/products.repository.spec.ts
@@ -1,4 +1,4 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getRepository } from 'typeorm';
 
 import Product from '../entities/product';
 import ProductRepository from './products.repository';
@@ -16,7 +16,19 @@ describe('product repository', () => {
   test('create a entry in database', async () => {
     const productRepository = new ProductRepository();
     await productRepository.add({ name: 'Boneca', price: 22.5, quantity: 15 });
-    const product = await productRepository.findByName('Boneca');
+    const product = await getRepository(Product).findOne({
+      where: { name: 'Boneca' },
+    });
+    expect(product?.price).toBe(22.5);
+  });
+  test('try to find a entry in database by name', async () => {
+    const productRepository = new ProductRepository();
+    await productRepository.add({
+      name: 'Carrinho',
+      price: 22.5,
+      quantity: 15,
+    });
+    const product = await productRepository.findByName('Carrinho');
     expect(product?.price).toBe(22.5);
   });
 });
