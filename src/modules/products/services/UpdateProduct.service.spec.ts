cf19b35e99525062ec6b126d6d372d1be09df7a1
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-13 20:42:55 -0300

    feat: update product service return an error if repeats product name

--- src/modules/products/services/UpdateProduct.service.spec.ts
+++ src/modules/products/services/UpdateProduct.service.spec.ts
@@ -14,11 +14,18 @@ describe('CreateProductService', () => {
   });
   beforeEach(async () => {
     await getRepository(Product).delete('WHERE id==*');
-    await getRepository(Product).insert({
-      name: 'produto 1',
-      price: 22.5,
-      quantity: 15,
-    });
+    Promise.all([
+      getRepository(Product).insert({
+        name: 'produto 1',
+        price: 22.5,
+        quantity: 15,
+      }),
+      getRepository(Product).insert({
+        name: 'produto 2',
+        price: 22.5,
+        quantity: 15,
+      }),
+    ]);
   });
   test('Update the product name', async () => {
     const foundProduct = await getRepository(Product).findOne({
@@ -82,5 +89,24 @@ describe('CreateProductService', () => {
     expect(productAfter?.name).toBe(foundProduct?.name);
     expect(productAfter?.price).toBe(foundProduct?.price);
     expect(productAfter?.quantity).toBe(10);
+  });
+  test('should return error if try to rename a product with a taken name', async () => {
+    const foundProduct = await getRepository(Product).findOne({
+      where: {
+        name: 'produto 1',
+      },
+    });
+    const foundProductId = foundProduct?.id ?? '';
+    const updateProductService = new UpdateProductService();
+    let error = false;
+    try {
+      await updateProductService.execute(foundProductId, {
+        name: 'produto 2',
+      });
+    } catch (e) {
+      error = true;
+      expect(e.statusCode).toBe(409);
+    }
+    expect(error).toBeTruthy();
   });
 });
--- src/modules/products/services/UpdateProduct.service.ts
+++ src/modules/products/services/UpdateProduct.service.ts
@@ -1,3 +1,4 @@
+import AppError from '../../../shared/errors/AppError';
 import ProductRepository from '../typeorm/repositories/products.repository';
 
 interface ProductUpdate {
@@ -12,6 +13,14 @@ export default class UpdateProductService {
     updateProperties: ProductUpdate,
   ): Promise<void> {
     const productsRepository = new ProductRepository();
+    if (updateProperties?.name) {
+      const foundProduct = await productsRepository.findByName(
+        updateProperties.name,
+      );
+      if (foundProduct) {
+        throw new AppError('error, this product is already registred', 409);
+      }
+    }
     await productsRepository.modify(id, updateProperties);
   }
 }
