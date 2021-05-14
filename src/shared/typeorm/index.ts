cf7d78dde12e45c5a1284e44192a181d90fcb85f
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-14 00:40:19 -0300

    chore: typeorm connection config changed to their index file

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,3 +1,16 @@
 import { createConnection } from 'typeorm';
+import Product from '../../modules/products/typeorm/entities/product';
 
-createConnection();
+createConnection({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: 'admin',
+  entities: [Product],
+  database: 'database',
+  migrations: ['src/shared/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: 'src/shared/typeorm/migrations',
+  },
+});
