6fbd32e11f264a80423d128c91184cf94d233def
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-15 03:34:40 -0300

    feat: add validator in create a product route

--- src/shared/routes/products.routes.ts
+++ src/shared/routes/products.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+
 import ProductsController from '../../modules/products/controllers/products.controller';
+import createProductDTO from '../dtos/createProduct.dto';
+import validate from '../middlewares/validator';
 
 const router = Router();
 const productsController = new ProductsController();
 
 router.get('/products', productsController.getAll);
-router.post('/products', productsController.create);
+router.post('/products', validate(createProductDTO), productsController.create);
 router.get('/products/:id', productsController.details);
 router.patch('/products/:id', productsController.update);
 router.delete('/products/:id', productsController.delete);
